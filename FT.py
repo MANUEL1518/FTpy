e23f61fa4c352ecab127a5cb85763967026560fc
MANUEL1518 <loquendo_118_loco@hotmail.com>
2019-08-07 02:34:59 -0500

    V2.0

--- FT.py
+++ FT.py
@@ -1,102 +1,159 @@
 ﻿import ftplib
+import getpass
 import time
+import sys
 import os
-#definir comandos
-def comandos():
-    while True:
-        try:
-            print("=================================================")
-            print("          Comandos que puedes usar:")
-            print("")
-            print("       cd: coloca en un directorio")
-            print("       ls: enlista contenido de directorio")
-            print("       put: sube archivos a servidor")
-            print("       get: baja archivo de servidor")
-            print("       mget: baja archivos en masa")
-            print("       mkdir: crea nueva carpeta")
-            print("       delete: borra ficheros de directorio")
-            print("       rm: borra carpetas")
-            print("       bye: cierra conexion")
-            print("")
-            print("=================================================")
-            while True:
-                opc = input("ftp>")
-                if opc=="cd":
-                    direc=input("(directorio)")
-                    ftp.cwd(direc)
-                else:
-                    if opc=="ls":
-                        ftp.dir()
-                    else:
-                        if opc=="put":
-                            filename=input("(file name)")
-                            f = open(filename, 'rb')
-                            print("uploading...")
-                            ftp.storbinary('STOR ' + filename, f)
-                            f.close()
-                        else:
-                            if opc=="get":
-                                filename=input("(file name)")
-                                print("downloading...")
-                                ftp.retrbinary('RETR '+filename, open(filename, 'wb').write)
-                            else:
-                                if opc=="mget":
-                                    filenames = ftp.nlst()
-                                    print("downloading...")
-                                    for filename in filenames:
-                                        local_filename = './' + "'" + filename + "'"
-                                        file = open(local_filename, 'wb')
-                                        ftp.retrbinary('RETR '+ filename, file.write)
-                                    archivos = os.listdir(".")
-                                    for archivo in archivos:
-                                        new = archivo.replace("'", "")
-                                        nombre = new.replace(" ", "_")
-                                        os.rename(archivo, nombre)
-                                else:
-                                    if opc=="delete":
-                                        file = input("(file name)")
-                                        ftp.delete(file)
-                                    else:
-                                        if opc=="rm":
-                                            directorio = input("(directorio)")
-                                            ftp.rmd(directorio)
-                                        else:
-                                            if opc=="mkdir":
-                                                directorio = input("(nombre de nuevo directorio)")
-                                                ftp.mkd(directorio)
-                                            else:
-                                                if opc=="bye":
-                                                    file.close()
-                                                    break
-                                                else:
-                                                    print(opc,"No es un comando elegible")
-        except:
-            print("no hay conexion")
-            ftp.login(nickwite,BEto123321)
 
-#Menu de inicio
-print("==================Conectar_FTP===================")
-print("")
-host = input("Escribe el nombre del servidor: ")
+def command():
+	print("=================================================")
+	print("          Comandos que puedes usar: ")
+	print("")
+	print(" cd:     coloca en un directorio")
+	print(" ls:     enlista contenido de directorio")
+	print(" put:    sube archivos a servidor")
+	print(" get:    baja archivo de servidor")
+	print(" mget:   baja archivos en masa")
+	print(" mkdir:  crea nueva carpeta")
+	print(" delete: borra ficheros de directorio")
+	print(" rm:     borra carpetas")
+	print(" bye:    cierra conexion")
+	print("")
+	print("=================================================")
+
+def comandos():
+	try:
+		intro_text = input("ftp>")
+		intro = intro_text.split(" ")
+		opc = intro[0]
+		if opc=="cd":
+			try:
+				direc = intro[1]
+			except:
+				direc = input("(nombre de carpeta) ")
+			try:
+				ftp.cwd(direc)
+			except:
+				print("No hay un directorio llamado asi")
+			comandos()
+		else:
+			if opc=="ls":
+				ftp.dir()
+				comandos()
+			else:
+				if opc=="put":
+					try:
+						filename = intro[1]
+					except:
+						filename = input("(file name) ")
+					f = open(filename, 'rb')
+					try:
+						print("uploading...")
+						ftp.storbinary('STOR ' + filename, f)
+						f.close()
+					except:
+						print("No se encontro un archivo con este nombre")
+					comandos()
+				else:
+					if opc=="get":
+						try:
+							filename = intro[1]
+						except:
+							filename = input("(file name) ")
+						try:
+							print("downloading...")
+							ftp.retrbinary('RETR '+filename, open(filename, 'wb').write)
+						except:
+							print("No se encontro un archivo con este nombre")
+						comandos()
+					else:
+						if opc=="mget":
+							filenames = ftp.nlst()
+							print("downloading...")
+							for filename in filenames:
+								local_filename = './' + "'" + filename + "'"
+								file = open(local_filename, 'wb')
+								ftp.retrbinary('RETR '+ filename, file.write)
+							archivos = os.listdir(".")
+							for archivo in archivos:
+								new = archivo.replace("'", "")
+								nombre = new.replace(" ", "_")
+								os.rename(archivo, nombre)
+							comandos()
+						else:
+							if opc=="delete":
+								try:
+									file = intro[1]
+								except:
+									file = input("(file name) ")
+								ftp.delete(file)
+								comandos()
+							else:
+								if opc=="rm":
+									try:
+										directorio = intro[1]
+									except:
+										directorio = input("(directorio) ")
+									try:
+										ftp.rmd(directorio)
+									except:
+										print("No se encontro un folder con este nombre")
+									comandos()
+								else:
+									if opc=="mkdir":
+										try:
+											directorio = intro[1]
+										except:
+											directorio = input("(nombre de nuevo directorio) ")
+										try:
+											ftp.mkd(directorio)
+										except:
+											print("No se pudo crear folder")
+										comandos()
+									else:
+										if opc=="bye" or opc=="exit":
+											file.close()
+											print("Good Bye")
+										else:
+											if opc=="help":
+												command()
+												comandos()
+											else:
+												if opc=="cls" or opc=="clear":
+													try:
+														try:
+															os.system("cls")
+														except:
+															os.system("clear")
+													except:
+														print("Este comando no es compatible")
+													comandos()
+												else:
+													print(opc," No es un comando elegible")
+													comandos()
+	except:
+		print("no hay conexion")
+		try:
+			ftp.login(user, pasd)
+			comandos()
+		except:
+			#Usuario o contraseña incorrectos
+			print("Conexion cerrada")
+
+host = sys.argv[1]
 try:
     ftp = ftplib.FTP(host)
     #Si se pudo conectar al servidor
-    print("Se ha podido conectar excitosamente el host")
-    print("=================================================")
-    user = input("Ingresa tu Usuario: ")
-    pasd = input("Ingresa tu Contrasena: ")
+    user = sys.argv[2]
+    pasd = getpass.getpass("Contraseña: ")
     try:
         ftp.login(user, pasd)
         #Si se pudo acceder con excito
-        print("Se ha podido conectar excitosamente tu espacio")
-        print("=================================================")
+        print("[OK]")
+        command()
         comandos()
     except:
         #Usuario o contraseña incorrectos
-        print("=================================================")
-        print("Usuario o contrasena incorrecta :c")
+        print("### Usuario o contraseña incorrecta ###")
 except:
     #Problema al acceder a servidor
-    print("=================================================")
-    print("Hubo un error. Asegurate que ayas escrito bien el servidor")
-
+    print("### [ERROR] Sintaxis FT.py [HOST] [USER] ###")
